a5d38ef538785baf991796086bd7fb6ae7820aa8
josemal37 <josemal_37@yahoo.es>
2017-02-22 09:18:41 -0400

    eliminado codigo antiguo que fue refactorizado

---
+++ application/views/administrador/bienvenida.php
@@ -0,0 +1,19 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+<?php $this->load->view("base/header"); ?>
+
+<div class="text-center">
+
+	<h1><?= $titulo ?></h1>
+
+</div>
+
+<?php $this->load->view("base/menu"); ?>
+
+<div class="container">
+
+	<p>Bienvenido al sistema.</p>
+
+</div>
+
+<?php $this->load->view("base/footer"); ?>
