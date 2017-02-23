876d94a77e03b39832f096cce1d6587325441a2a
josemal37 <josemal_37@yahoo.es>
2017-02-23 10:14:08 -0400

    aniadido footer

--- application/views/base/footer.php
+++ application/views/base/footer.php
@@ -1,5 +1,13 @@
 <?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
 
+<div class="container text-center">
+
+	<hr class="small">
+	
+	<p class="text-muted">Copyright &copy; Fundación ATICA 2017</p>
+
+</div>
+
 </body>
 
 </html>
